298000b26b77c007c0074466ca3690b71908c8f6
nicolasdca <nicolasdavid193@gmail.com>
2023-10-29 18:46:48 -0300

    Fokus 2 Alura - Aula 5.1

--- script-crud.js
+++ script-crud.js
@@ -33,6 +33,9 @@ let tarefaEmEdicao = null;
 let paragraphEmEdicao = null;
 
 const selecionaTarefa = (tarefa, elemento) => {
+  if (tarefa.concluida) {
+    return;
+  }
   document
     .querySelectorAll('.app__section-task-list-item-active')
     .forEach(function (button) {
@@ -102,9 +105,13 @@ function createTask(tarefa) {
   };
 
   svgIcon.addEventListener('click', (event) => {
-    event.stopPropagation();
-    button.setAttribute('disabled', true);
-    li.classList.add('app__section-task-list-item-complete');
+    if (tarefa == tarefaSelecionada) {
+      event.stopPropagation();
+      button.setAttribute('disabled', true);
+      li.classList.add('app__section-task-list-item-complete');
+      tarefaSelecionada.concluida = true;
+      updateLocalStorage();
+    }
   });
 
   if (tarefa.concluida) {
@@ -153,4 +160,15 @@ formTask.addEventListener('submit', (evento) => {
   }
   updateLocalStorage();
   limparForm();
+});
+
+document.addEventListener('TarefaFinalizada', function (e) {
+  if (tarefaSelecionada) {
+    tarefaSelecionada.concluida = true;
+    itemTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+    itemTarefaSelecionada
+      .querySelector('button')
+      .setAttribute('disabled', true);
+    updateLocalStorage();
+  }
 });
